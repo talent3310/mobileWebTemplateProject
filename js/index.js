2a6a90cfa499f9c684e34c0f41501dcca03c3bf3
deniz <deniz.aslan331@hotmail.com>
2016-05-20 05:38:37 +0300

    finish

--- js/index.js
+++ js/index.js
@@ -5,7 +5,7 @@
         $(e.target)
             .prev('.panel-heading')
             .find(".more-less")
-            .toggleClass('glyphicon-plus-sign glyphicon-minus-sign');
+            .toggleClass('glyphicon-chevron-right glyphicon-chevron-down');
     }
     $('.panel-group').on('hidden.bs.collapse', toggleIcon);
     $('.panel-group').on('shown.bs.collapse', toggleIcon);
